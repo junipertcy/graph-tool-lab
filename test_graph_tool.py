e7e6aba8499017810f858af02b22a3e52826a76c
Mosky <mosky.tw@gmail.com>
2014-05-15 01:51:08 +0800

    render the graph by closeness

--- test_graph_tool.py
+++ test_graph_tool.py
@@ -98,6 +98,17 @@ def render_graph(g, path='output/{}.pdf'):
     ))
     graph_draw(**arg_map)
 
+    # use closeness
+    v_closeness_p = g.vp['closeness']
+    v_closeness_size = prop_to_size(v_closeness_p, MI_V_SIZE, MA_V_SIZE)
+    closeness_arg_map = arg_map.copy()
+    closeness_arg_map.update(dict(
+        output = path.format('1-5-random-closeness'),
+        vertex_size = v_closeness_size,
+        vertex_fill_color = v_closeness_size,
+    ))
+    graph_draw(**closeness_arg_map)
+
     # sfdp_layout
     arg_map.update(dict(
         output = path.format('2-1-sfdp'),
